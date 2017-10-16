d5b8ee03939dafaba106b9fec2c933bf02910a39
Waldir J. Pereira Junior <waldirpereira@gmail.com>
2017-10-16 15:41:45 -0200

    Início do curso de Angular 4, seção 3, aula 7

---
+++ typescript/app.ts
@@ -0,0 +1,12 @@
+let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!"
+console.log(message)
+
+let episode: number = 4
+console.log("This is episode " + episode)
+
+episode = episode + 1
+console.log("Next episode is " + episode)
+
+let favoriteDroid: any
+favoriteDroid = "BB-8"
+console.log("My favorite droid is " + favoriteDroid)
---
+++ typescript/dist/app.js
@@ -0,0 +1,9 @@
+var message = "Help me, Obi-Wan Kenobi. You're my only hope!";
+console.log(message);
+var episode = 4;
+console.log("This is episode " + episode);
+episode = episode + 1;
+console.log("Next episode is " + episode);
+var favoriteDroid;
+favoriteDroid = "BB-8";
+console.log("My favorite droid is " + favoriteDroid);
