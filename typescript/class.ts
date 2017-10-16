bef4f07d15e71089654540dfbe1c564916aa0fd5
Waldir J. Pereira Junior <waldirpereira@gmail.com>
2017-10-16 20:50:53 -0200

    Seção 3, aula 15

--- typescript/class.ts
+++ typescript/class.ts
@@ -1,12 +1,14 @@
 import {Spacecraft, Containership} from './base-ships'
 import {MilleniumFalcon} from './starfighters'
 
+import * as _ from 'lodash'
+console.log(_.pad("Typescript Examples", 40, "-="))
+
 let ship = new Spacecraft('hyperdrive')
 ship.jumpIntoHyperspace()
 
 let falcon = new MilleniumFalcon()
 falcon.jumpIntoHyperspace()
-
 
 let goodForTheJob = (ship: Containership) => ship.cargoContainer > 2
 
--- typescript/dist/class.js
+++ typescript/dist/class.js
@@ -2,6 +2,8 @@
 exports.__esModule = true;
 var base_ships_1 = require("./base-ships");
 var starfighters_1 = require("./starfighters");
+var _ = require("lodash");
+console.log(_.pad("Typescript Examples", 40, "-="));
 var ship = new base_ships_1.Spacecraft('hyperdrive');
 ship.jumpIntoHyperspace();
 var falcon = new starfighters_1.MilleniumFalcon();
