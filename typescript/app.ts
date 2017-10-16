b7106852ac632dba013a7f1607433159a784ebf2
Waldir J. Pereira Junior <waldirpereira@gmail.com>
2017-10-16 15:48:53 -0200

    Seção 3, aula 9

--- typescript/app.ts
+++ typescript/app.ts
@@ -10,3 +10,23 @@ console.log("Next episode is " + episode)
 let favoriteDroid: any
 favoriteDroid = "BB-8"
 console.log("My favorite droid is " + favoriteDroid)
+
+let isEnoughToBeatMF = function (parsecs: number) : boolean {
+  return parsecs < 12
+}
+
+let distance = 11
+console.log(`Is ${distance} parsecs enough to beat Millenium Falcon? ${isEnoughToBeatMF(distance) ? 'YES' : 'NO'}`)
+
+let call = (name: string) => console.log(`Do you copy, ${name}?`)
+
+call('R2')
+
+let getInc = () => 1
+
+function inc (speed: number, inc: number = getInc()) : number {
+  return speed + inc
+}
+
+console.log(`inc(5, 1) = ${inc(5, 1)}`)
+console.log(`inc(5) = ${inc(5)}`)
--- typescript/dist/app.js
+++ typescript/dist/app.js
@@ -7,3 +7,17 @@ console.log("Next episode is " + episode);
 var favoriteDroid;
 favoriteDroid = "BB-8";
 console.log("My favorite droid is " + favoriteDroid);
+var isEnoughToBeatMF = function (parsecs) {
+    return parsecs < 12;
+};
+var distance = 11;
+console.log("Is " + distance + " parsecs enough to beat Millenium Falcon? " + (isEnoughToBeatMF(distance) ? 'YES' : 'NO'));
+var call = function (name) { return console.log("Do you copy, " + name + "?"); };
+call('R2');
+var getInc = function () { return 1; };
+function inc(speed, inc) {
+    if (inc === void 0) { inc = getInc(); }
+    return speed + inc;
+}
+console.log("inc(5, 1) = " + inc(5, 1));
+console.log("inc(5) = " + inc(5));
