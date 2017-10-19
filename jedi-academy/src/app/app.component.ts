f13f68e03cdb7c67af3327ca8fe8c2a228251675
Waldir J. Pereira Junior <waldirpereira@gmail.com>
2017-10-19 15:36:15 -0200

    Seção 4, aula 24

--- jedi-academy/src/app/app.component.ts
+++ jedi-academy/src/app/app.component.ts
@@ -5,5 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'jad';
+  luke = {name: 'Luke', isJedi: true, temple: 'Coruscant'}
+  han = {name: 'Han Solo', isJedi: false }
+  leia = {name: 'Leia', isJedi: false }
 }
