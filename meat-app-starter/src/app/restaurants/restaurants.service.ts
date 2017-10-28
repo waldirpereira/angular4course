cfcb504f413b49bfcca3d244be9aaddbbd997688
Waldir J. Pereira Junior <waldirpereira@gmail.com>
2017-10-27 22:34:22 -0200

    Seção 5, aula 51: implementando os itens de menu + pipe currency

--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
 
 import {Restaurant} from './restaurant/restaurant.model'
+import {MenuItem} from '../restaurant-detail/menu-item/menu-item.model'
 
 import {MEAT_API} from '../app.api'
 import {ErrorHandler} from '../app.error-handler'
@@ -29,6 +30,12 @@ export class RestaurantsService {
 
   reviewsOfRestaurant(id: string): Observable<any> {
     return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError)
   }
