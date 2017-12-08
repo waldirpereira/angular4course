d718a064dd360cb44f37146358a3c310f9f08f6e
Waldir J. Pereira Junior <waldirpereira@gmail.com>
2017-12-08 12:53:26 -0200

    Seção 6, aula 69: navegação progbramática via Router

--- meat-app-starter/src/app/order/order.component.ts
+++ meat-app-starter/src/app/order/order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
+
 import { RadioOption } from '../shared/radio/radio-option.model';
 import {OrderService} from './order.service'
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model'
@@ -18,7 +20,7 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão refeição', value: 'REF'}
   ]
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -46,7 +48,7 @@ export class OrderComponent implements OnInit {
       .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order)
       .subscribe((orderId: string) => {
-        console.log(`Compra concluida: ${orderId}`);
+        this.router.navigate(['/order-summary'])
         this.orderService.clear()
       })
   }
--- meat-app-starter/src/app/order/order.service.ts
+++ meat-app-starter/src/app/order/order.service.ts
@@ -40,7 +40,8 @@ export class OrderService {
     headers.append('Content-Type', 'application/json')
     return this.http.post(`${MEAT_API}/orders`,
       JSON.stringify(order),
-      new RequestOptions(headers))
-      .map(response => response.json())
+      new RequestOptions({headers: headers}))
+        .map(response => response.json())
+        .map(order => order.id)
   }
 }
